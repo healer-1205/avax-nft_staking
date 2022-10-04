096d6746fabeea222cdc92d7cc193d2b1fbab45d
healer-1205 <crazyhealer1205@gmail.com>
2022-10-04 19:59:57 -0400

    add mint contracts and make layout of journey page

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Route, Routes } from "react-router-dom"
 import { Home } from './pages/Home';
 import { Market } from './pages/Market';
 import { MyWoA } from './pages/MyWoA';
+import { Journey } from './pages/Journey';
 import './App.css';
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/market" element={<Market />} />
         <Route path="/mywoa" element={<MyWoA />} />
+        <Route path="/journey" element={<Journey />} />
       </Routes>
     </>
   );
---
+++ src/pages/Journey/Journey.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Header } from "../../components/Header";
+import { Footer } from "../../components/Footer";
+import "./Journey.css";
+
+export const Journey: React.FC = () => {
+	return (
+		<div className='journey-container'>
+			<Header />
+			<Footer />
+		</div>
+	)
+}
