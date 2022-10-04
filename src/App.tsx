f537c322b6b45a73028247529a1ca029ea872da9
healer-1205 <crazyhealer1205@gmail.com>
2022-10-04 13:41:02 -0400

    finish market page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Route, Routes } from "react-router-dom"
 import { Home } from './pages/Home';
+import { Market } from './pages/Market';
 import './App.css';
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/market" element={<Market />} />
       </Routes>
     </>
   );
