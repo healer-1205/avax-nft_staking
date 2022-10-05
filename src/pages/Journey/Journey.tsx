366c1657eb0268647e918a68a5ce51a66b2a2bb9
healer-1205 <crazyhealer1205@gmail.com>
2022-10-04 23:01:41 -0400

    make connect btn as a component&ts used contract

--- src/pages/Journey/Journey.tsx
+++ src/pages/Journey/Journey.tsx
@@ -1,13 +1,14 @@
-import React from "react";
-import { Header } from "../../components/Header";
-import { Footer } from "../../components/Footer";
-import "./Journey.css";
+import React from "react"
+import { Header } from "../../components/Header"
+import { Footer } from "../../components/Footer"
+import "./Journey.css"
 
 export const Journey: React.FC = () => {
-	return (
-		<div className='journey-container'>
-			<Header />
-			<Footer />
-		</div>
-	)
+  return (
+    <div className="journey-container">
+      <Header />
+      <div></div>
+      <Footer />
+    </div>
+  )
 }
