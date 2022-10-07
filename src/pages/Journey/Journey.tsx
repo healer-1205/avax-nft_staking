f5c93c273ad540e8e946734d142d7da59b19f836
healer-1205 <crazyhealer1205@gmail.com>
2022-10-07 10:18:38 -0400

    spell check

--- src/pages/Journey/Journey.tsx
+++ src/pages/Journey/Journey.tsx
@@ -27,34 +27,37 @@ export const Journey: React.FC = () => {
   })
 
   const stakingContract = useContract<ERC721Staking>({
-    addressOrName: config.stakingContractAddrss,
+    addressOrName: config.stakingContractAddress,
     signerOrProvider: isConnected ? signerData : provider,
     contractInterface: ERC721StakingABI,
   })
 
   useEffect(() => {
     if (!nftContract.signer) return
-    nftContract.signer.getAddress().then((_address) => {
-      nftContract.balanceOf(_address).then((_balance) => {
-        const promises: Array<Promise<NFTTokenType>> = []
-        for (let index = ethers.BigNumber.from(0); index.lt(_balance); index = index.add(1))
-          promises.push(
-            new Promise<NFTTokenType>(async (resolve, reject) => {
-              const _tokenId = await nftContract.tokenOfOwnerByIndex(_address, index)
-              const _approvedAddress = await nftContract.getApproved(_tokenId)
-              resolve({ id: _tokenId, approved: _approvedAddress === config.stakingContractAddrss })
+    nftContract.signer
+      .getAddress()
+      .then((_address) => {
+        nftContract.balanceOf(_address).then((_balance) => {
+          const promises: Array<Promise<NFTTokenType>> = []
+          for (let index = ethers.BigNumber.from(0); index.lt(_balance); index = index.add(1))
+            promises.push(
+              new Promise<NFTTokenType>(async (resolve, reject) => {
+                const _tokenId = await nftContract.tokenOfOwnerByIndex(_address, index)
+                const _approvedAddress = await nftContract.getApproved(_tokenId)
+                resolve({ id: _tokenId, approved: _approvedAddress === config.stakingContractAddress })
+              })
+            )
+          Promise.allSettled(promises).then((_tokenIdResults) => {
+            const _tokenIds = _tokenIdResults.flatMap((_tokenIdResult) => {
+              if (_tokenIdResult.status === "fulfilled")
+                return [(_tokenIdResult as PromiseFulfilledResult<NFTTokenType>).value]
+              else return []
             })
-          )
-        Promise.allSettled(promises).then((_tokenIdResults) => {
-          const _tokenIds = _tokenIdResults.flatMap((_tokenIdResult) => {
-            if (_tokenIdResult.status === "fulfilled")
-              return [(_tokenIdResult as PromiseFulfilledResult<NFTTokenType>).value]
-            else return []
+            setNftTokens(_tokenIds)
           })
-          setNftTokens(_tokenIds)
         })
       })
-    })
+      .catch(console.error)
   }, [nftContract])
 
   return (
@@ -77,7 +80,7 @@ export const Journey: React.FC = () => {
                         .catch(console.error)
                     else
                       nftContract
-                        .approve(config.stakingContractAddrss, _nftToken.id)
+                        .approve(config.stakingContractAddress, _nftToken.id)
                         .then(() => console.log("success"))
                         .catch(console.error)
                   }}
